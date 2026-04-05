7bb371809d7f4ca63539a92d3d62524a790c386a
shellghost-svg <guguangyi097@qq.com>
2026-04-05 23:56:19 +0800

    fix: add compose delegate imports

--- app/src/main/java/com/ghost/autoclicker/ui/main/MainViewModel.kt
+++ app/src/main/java/com/ghost/autoclicker/ui/main/MainViewModel.kt
@@ -7,6 +7,8 @@ import android.net.Uri
 import android.provider.Settings
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 import androidx.lifecycle.AndroidViewModel
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
